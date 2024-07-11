4d96b3d38a0b9feeb50a465e5dcad83bb19382d0
rulkimi <azrulkimi00@gmail.com>
2024-07-11 21:17:05 +0800

    added scroll to menu

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <TheNavbar />
-  <TheHero text="HI, I'M KIMI" />
+  <section ref="hero">
+    <TheHero text="HI, I'M KIMI" />
+  </section>
   <div
     class="fixed text-text right-4 bottom-4 lg:right-1 lg:text-[1.44rem] cursor-pointer font-bold hover:text-[1.5rem] transition-all duration-300 ease-in-out"
     style="writing-mode: vertical-lr;">
     Scroll here -->
   </div>
-  <AboutMe />
+  <section ref="about">
+    <AboutMe />
+  </section>
 </template>
 
 <script setup>
+import { ref, provide, onMounted } from 'vue';
 import TheHero from './components/TheHero.vue';
 import TheNavbar from './components/TheNavbar.vue';
 import AboutMe from './components/AboutMe.vue';
+
+const hero = ref(null);
+const about = ref(null);
+const refs = { hero, about };
+
+provide('sectionRefs', refs);
+
+onMounted(() => {
+  console.log(hero.value)
+})
 </script>
 
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="fixed bg-secondary/[0.8] pb-2 w-full z-20 top-0 start-0">
+  <nav ref="navbar" class="fixed bg-secondary/[0.8] pb-2 w-full z-20 top-0 start-0">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="#" class="flex items-center space-x-5 rtl:space-x-reverse">
         <img src="../assets/kimi.svg" width="30" />
@@ -9,8 +9,12 @@
         <div class="flex items-center">
           <ul class="items-center font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse">
             <li v-for="menu in menus">
-              <a href="#" class="block py-2 px-3 text-[1.44rem] text-text font-bold hover:text-accent md:p-0">
-                {{ menu }}
+              <a
+                href="#"
+                class="block py-2 px-3 text-[1.44rem] text-text font-bold hover:text-accent md:p-0"
+                @click="scroll(menu.ref)"
+              >
+                {{ menu.name }}
               </a>
             </li>
             <BaseButton>Let's talk!</BaseButton>
@@ -22,10 +26,30 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, inject } from 'vue';
 import BaseButton from './BaseButton.vue';
 
+const navbar = ref(null);
 const menus = ref([
-  'About', 'Projects', 'Aspirations'
+  { name: 'About', ref: 'about'},
+  { name: 'Projects', ref: 'projects'},
+  { name: 'Aspirations', ref: 'aspirations'},
 ]);
+
+const sectionRefs = inject('sectionRefs');
+
+const scroll = (refName) => {
+  if (sectionRefs[refName]?.value) {
+    const element = sectionRefs[refName].value;
+    const navbarHeight = navbar.value.offsetHeight || 0;
+
+    // Calculate the offset position
+    const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+    const offsetPosition = elementPosition - navbarHeight;
+
+    window.scrollTo({ top: offsetPosition, behavior: 'smooth' });
+  }
+};
+
+
 </script>
