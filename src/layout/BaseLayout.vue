40924b86d1dd1e996c55ed03ad207401c6fbd232
rulkimi <azrulkimi00@gmail.com>
2024-09-14 23:25:37 +0800

    remove unnecessary h-full and w-full

--- src/layout/BaseLayout.vue
+++ src/layout/BaseLayout.vue
@@ -5,7 +5,7 @@ import { GlowingBackground } from '@/components/templates';
 
 <template>
   <GlowingBackground color="#0984FF">
-    <div class="max-w-[1280px] mx-auto h-screen flex gap-4 px-24 w-full h-full">
+    <div class="max-w-[1280px] mx-auto h-screen flex gap-4 px-24">
       <NavBar class="flex-1"/>
       <div class="flex-1 flex flex-col gap-28 py-24">
         <slot></slot>
