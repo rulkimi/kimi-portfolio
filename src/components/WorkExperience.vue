a17ec08a171f8ef6ba78528254c5efb8df7d46bf
rulkimi <azrulkimi00@gmail.com>
2024-09-14 23:55:53 +0800

    fix absolute positioning issue

--- src/components/WorkExperience.vue
+++ src/components/WorkExperience.vue
@@ -3,26 +3,24 @@ import { HoverCard } from '@/components/templates';
 </script>
 
 <template>
-  <div class="relative">
-    <div class="absolute -inset-x-6 flex flex-col gap-4">
-      <HoverCard
-        date="2024 &ndash; PRESENT"
-        title="Software Engineer"
-        company="Mesiniaga Digital Sdn. Bhd."
-        :tags="['Vue.Js', 'Vite', 'Figma', 'GenAI']"
-      >
-        Refined and built Mesiniaga’s first AI-integrated website, incorporating chatbots, translators, and analysis tools,
-        while designing the UI and API integration for admin pages to manage users, prompt templates, and message logs with a simple CRUD flow.
-      </HoverCard>
-      <HoverCard
-        date="2022 &ndash; 2023"
-        title="Software Trainee"
-        company="Knex Inc."
-        :tags="['Dataplicity', 'Ubuntu']"
-      >
-        Monitored the company's IoT devices across 6 regions in Brazil, executed 10+ code modifications for tasks like storage optimization and error checks,
-        and performed initial debugging on 5+ software issues, improving functionality and user experience.
-      </HoverCard>
-    </div>
+  <div class="relative -inset-x-6 flex flex-col gap-4">
+    <HoverCard
+      date="2024 &ndash; PRESENT"
+      title="Software Engineer"
+      company="Mesiniaga Digital Sdn. Bhd."
+      :tags="['Vue.Js', 'Vite', 'Figma', 'GenAI']"
+    >
+      Refined and built Mesiniaga’s first AI-integrated website, incorporating chatbots, translators, and analysis tools,
+      while designing the UI and API integration for admin pages to manage users, prompt templates, and message logs with a simple CRUD flow.
+    </HoverCard>
+    <HoverCard
+      date="2022 &ndash; 2023"
+      title="Software Trainee"
+      company="Knex Inc."
+      :tags="['Dataplicity', 'Ubuntu']"
+    >
+      Monitored the company's IoT devices across 6 regions in Brazil, executed 10+ code modifications for tasks like storage optimization and error checks,
+      and performed initial debugging on 5+ software issues, improving functionality and user experience.
+    </HoverCard>
   </div>
 </template>
