c8fb2bee8d3dbf47789556e88e2105fdf42a01d3
rulkimi <azrulkimi00@gmail.com>
2024-09-14 22:31:38 +0800

    correct background

--- src/layout/BaseLayout.vue
+++ src/layout/BaseLayout.vue
@@ -5,7 +5,7 @@ import { GlowingBackground } from '@/components/templates';
 
 <template>
   <GlowingBackground color="#0984FF">
-    <div class="max-w-[1280px] mx-auto h-screen flex gap-4 px-24 w-full h-full overflow-auto custom-scroll">
+    <div class="max-w-[1280px] mx-auto h-screen flex gap-4 px-24 w-full h-full">
       <NavBar class="flex-1"/>
       <div class="flex-1 flex flex-col gap-28 py-24">
         <slot></slot>
@@ -13,20 +13,3 @@ import { GlowingBackground } from '@/components/templates';
     </div>
   </GlowingBackground>
 </template>
-
-<style scoped>
-/* Custom scrollbar styling */
-.custom-scroll::-webkit-scrollbar {
-  width: 6px;
-  background-color: transparent;
-}
-
-.custom-scroll::-webkit-scrollbar-thumb {
-  background-color: #ccc;
-  border-radius: 4px;
-}
-
-.custom-scroll::-webkit-scrollbar-thumb:hover {
-  background-color: #aaa;
-}
-</style>
