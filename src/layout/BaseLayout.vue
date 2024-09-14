ec52d37753324b3a1f75b361f58827fc6874414b
rulkimi <azrulkimi00@gmail.com>
2024-09-14 23:28:27 +0800

    use 2-1/2 instead of flex-1

--- src/layout/BaseLayout.vue
+++ src/layout/BaseLayout.vue
@@ -6,8 +6,8 @@ import { GlowingBackground } from '@/components/templates';
 <template>
   <GlowingBackground color="#0984FF">
     <div class="max-w-[1280px] mx-auto h-screen flex gap-4 px-24">
-      <NavBar class="flex-1"/>
-      <div class="flex-1 flex flex-col gap-28 py-24">
+      <NavBar class="w-1/2"/>
+      <div class="w-1/2 flex flex-col gap-28 py-24">
         <slot></slot>
       </div>
     </div>
