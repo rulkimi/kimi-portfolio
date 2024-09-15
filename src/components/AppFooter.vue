7c1d5aa2ebed5e222592ae094928f032a9d4c931
rulkimi <azrulkimi00@gmail.com>
2024-09-15 15:58:19 +0800

    use s instead of strike tag

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="pb-20">
-    <strike>A clone of</strike> Heavily inspired by <a href="https://brittanychiang.com/" target="_blank">Brittany Chiang's Portfolio</a> and coded in
+    <s>A clone of</s> Heavily inspired by <a href="https://brittanychiang.com/" target="_blank">Brittany Chiang's Portfolio</a> and coded in
     <a href="https://code.visualstudio.com/" target="_blank">Visual Studio Code</a>.
     Built with <a href="https://vuejs.org/" target="_blank">Vue.js</a> and <a href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a>,
     deployed with <a href="https://pages.github.com/" target="_blank">Github Pages</a>. 
